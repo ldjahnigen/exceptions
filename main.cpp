f382cebb1f68f36a311a3863ae27314a31958a64
Louis <ldjahnigen@gmail.com>
2023-11-28 10:27:31 -0500

    added exceptions and basic handling

--- main.cpp
+++ main.cpp
@@ -4,15 +4,44 @@
 
 #include <iostream>
 
+class invalidCharacterException : public std::exception {
+  public:
+    std::string message = "an invalid character was passed";
+
+};
+
+
+class invalidRangeException : public std::exception {
+  public:
+    std::string message = "an invalid range was passed";
+
+};
+
+
 char character(char start, int offset);
 
 int main() {
-  std::cout << character('a', 1) << '\n';
+  try {
+    std::cout << character('a', 1) << '\n';
+  } catch (invalidRangeException e) {
+    std::cout << e.message << '\n';
+  } catch (invalidCharacterException e) {
+    std::cout << e.message << '\n';
+  }
 
   return 0;
 }
 
 
 char character(char start, int offset) {
+  if (!isalpha(start)) {
+    invalidCharacterException e;
+    throw e;
+  }
+  if (offset < 0 || offset > 26) {
+    invalidRangeException e;
+    throw e;
+  }
   return start + offset;
 }
+
